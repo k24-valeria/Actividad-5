b0724cd6df0c4dc39f4fa42de7d254d11da29856
k24-valeria <karlaleyva2003@hotmail.com>
2021-04-26 18:16:41 -0500

    Ejercicio terminado

---
+++ src/Ejercicio.java
@@ -0,0 +1,24 @@
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+        int num1=20;
+        int num2=18;
+        int sum= num1 + num2;
+        int res= num1 - num2;
+        int mul= num1 * num2;
+        int div= num1 / num2;
+        int mod= num1 % num2;
+
+
+
+        System.out.println("El num1 es: "+num1+" El num2 es: "+num2);
+        System.out.println("La sum es: "+sum);
+        System.out.println("La res es: "+res);
+        System.out.println("La mul es: "+mul);
+        System.out.println("La div es: "+div);
+        System.out.println("El mod es: "+mod);
+
+    }
+}
+
